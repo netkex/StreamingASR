0b5c80383c4fb8fd8904b6cf0517e0afe952c012
netkex <dp.3481088@gmail.com>
2025-03-13 14:35:38 +0300

    [WIP] Add WavTokensEncoder to model __init__.py

--- StreamASRLib/StreamASRLib/model/__init__.py
+++ StreamASRLib/StreamASRLib/model/__init__.py
@@ -2,3 +2,4 @@ from .config import DEVICE
 from .ASRBaseModel import ASRBaseModel
 from .CustomWavTokenizer import CustomWavTokenizer
 from .WavQwenModel import ExtendedEmbedding, WavQwenModel
+from .WavTokensEncoder import WavTokensEncoder
