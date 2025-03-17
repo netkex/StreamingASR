c9676f7ea9718975b1e9494bce695bdec2a789dd
netkex <dp.3481088@gmail.com>
2025-03-18 02:44:34 +0300

    [WIP] Add wav-qwen model with custom encoder

---
+++ StreamASRLib/StreamASRLib/model/WavQwenModelWithEncoder.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+from safetensors import safe_open
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from StreamASRLib.model import WavTokensEncoder
+
+
+class EmbeddingWithEncoder(nn.Module):
+    '''
+    Custom embedding class for wav-tokens with encoder
+    '''
+
+    def __init__(self, base_emb: nn.Embedding, wav_encoder: nn.Module, ext_token_threshold: int):
+        super().__init__()
+        self.base_emb = base_emb
+        self.hidden_size = base_emb.embedding_dim
+        self.wav_encoder = wav_encoder
+        self.ext_token_threshold = ext_token_threshold
+
+    def freeze_base_emb(self):
+        for param in self.base_emb.parameters():
+            param.requires_grad = False
+
+    def forward(self, input_tokens: torch.Tensor) -> torch.Tensor:
+        ext_token_mask = (input_tokens >= self.ext_token_threshold)
+        inp_emb = torch.zeros(list(
+            input_tokens.shape) + [self.hidden_size], requires_grad=True).to(self.base_emb.weight.device)
+        inp_emb[~ext_token_mask] = self.base_emb(input_tokens[~ext_token_mask])
+
+        wav_emb = []
+        for i in range(input_tokens.shape[0]):
+            wav_input = input_tokens[i][ext_token_mask[i]].reshape(
+                1, -1) - self.ext_token_threshold
+            wav_emb.append(self.wav_encoder(
+                wav_input).reshape(-1, self.hidden_size))
+        inp_emb[ext_token_mask] = torch.concatenate(wav_emb, dim=0)
+        return inp_emb
+
+
+class WavQwenModelWithEncoder:
+    QWEN_REPO = 'Qwen/Qwen2.5-0.5B'
+    EOS_TOKEN = 151643                               # taken from qwen tokenizer
+    AUDIO_TOKENS = 4096                              # taken from wav tokenizer doc
+    AUDIO_TOKEN_THRESHOLD = 151936                   # taken from qwen tokenizer
+    PADDING_TOKEN = EOS_TOKEN
+    BOA_TOKEN = AUDIO_TOKEN_THRESHOLD + AUDIO_TOKENS
+    EOA_TOKEN = BOA_TOKEN + 1
+    EXT_TOKENS = AUDIO_TOKENS + 2
+
+    def __init__(self, model, fast_tokenizer=True):
+        self.model = model
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            WavQwenModelWithEncoder.QWEN_REPO, use_fast=fast_tokenizer)
+
+    def freeze_qwen(self):
+        for parameter in self.model.model.parameters():
+            parameter.requires_grad = False
+        for parameter in self.model.model.embed_tokens.parameters():
+            parameter.requires_grad = True
+        self.model.model.embed_tokens.freeze_base_emb()
+
+    @classmethod
+    def init_with_encoder(cls, path: str, **encoder_kwargs):
+        wav_encoder = WavTokensEncoder.load(
+            path,
+            num_tokens=WavQwenModelWithEncoder.EXT_TOKENS,
+            **encoder_kwargs
+        )
+        qwen_model = AutoModelForCausalLM.from_pretrained(
+            WavQwenModelWithEncoder.QWEN_REPO)
+        base_emb = qwen_model.model.embed_tokens
+
+        enc_emb = EmbeddingWithEncoder(
+            base_emb, wav_encoder, WavQwenModelWithEncoder.AUDIO_TOKEN_THRESHOLD)
+        qwen_model.model.embed_tokens = enc_emb
+        return cls(qwen_model)
--- StreamASRLib/StreamASRLib/model/__init__.py
+++ StreamASRLib/StreamASRLib/model/__init__.py
@@ -3,3 +3,4 @@ from .ASRBaseModel import ASRBaseModel
 from .CustomWavTokenizer import CustomWavTokenizer
 from .WavQwenModel import ExtendedEmbedding, WavQwenModel
 from .WavTokensEncoder import WavTokensEncoder
+from .WavQwenModelWithEncoder import WavQwenModelWithEncoder
