c7e7df0a3cfb6b9a60dbc5daa4c8093a3bf386d7
netkex <dp.3481088@gmail.com>
2025-03-18 03:10:51 +0300

    [WIP] Refactor experiments

--- experiments/training_scripts/train_qwen_aug.py
+++ experiments/training_scripts/train_qwen_aug.py
@@ -1,24 +1,18 @@
 import argparse
-import wandb
 import os
-import multiprocessing
-
-import numpy as np
 
 import torch
 import torchaudio
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+import wandb
+from datasets import concatenate_datasets, load_dataset, load_from_disk
+from transformers import (
+    AutoTokenizer, DataCollatorWithPadding, TrainingArguments)
 
-from transformers import AutoTokenizer
-from transformers import Trainer, TrainingArguments, DataCollatorWithPadding, EvalPrediction
-from transformers.integrations import WandbCallback
-from datasets import load_dataset, load_from_disk, concatenate_datasets
-
-from StreamASRLib.eval import calculate_wer
-from StreamASRLib.dataset import MergeDataset, WavTextDataset, LibriDataset, SpecAugmentation, SpecAugmentationConfig
+from StreamASRLib.dataset import (LibriDataset, MergeDataset, SpecAugmentation,
+                                  SpecAugmentationConfig, WavTextDataset)
+from StreamASRLib.logging import WandbAudioSampleCallback
 from StreamASRLib.model import CustomWavTokenizer, WavQwenModel
-
+from StreamASRLib.training import WavAudioTrainer, WerMetric
 
 WANDB_TOKEN = os.environ['WANDB_TOKEN']
 os.environ['WANDB_PROJECT'] = 'Qwen05b-hf-full-train'
@@ -111,125 +105,6 @@ def load_val_ds(
         add_text=False
     )
     return wav_test_ds
-
-
-def build_ref_seq(tokenizer, output: torch.Tensor) -> str:
-    tokens = torch.argmax(output, dim=-1)
-    return tokenizer.decode(tokens.cpu())
-
-
-class CustomTrainer(Trainer):
-    '''
-    Custom trainer class with cross-entropy loss only for text tokens
-    '''
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.loss = nn.CrossEntropyLoss()
-
-    def compute_loss(self, model, inputs, return_outputs=False):
-        label_text_start = inputs.label_text_start
-        label_text_end = inputs.label_text_end
-        output = model(input_ids=inputs.input_ids,
-                       attention_mask=inputs.attention_mask)
-
-        logits = output.logits.float()
-        batch_size = logits.shape[0]
-        loss = 0
-
-        for id, (start, end) in enumerate(zip(label_text_start, label_text_end)):
-            seq_logits = logits[id, (start - 1):(end - 1), :]
-            seq_target = inputs.input_ids[id, start:end]
-            loss += self.loss(seq_logits, seq_target)
-        loss /= batch_size
-
-        return (loss, output) if (return_outputs) else loss
-
-
-# Metrics and logging stuff
-class WerMetric:
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
-        self.wer_hist = []
-
-    def __call__(self, eval_pred: EvalPrediction, compute_result: bool) -> float:
-        logits = eval_pred.predictions
-        input_ids = getattr(eval_pred, 'inputs')['input_ids']
-        label_text_start = eval_pred.label_ids[0]
-        label_text_end = eval_pred.label_ids[1]
-
-        for id, (start, end) in enumerate(zip(label_text_start, label_text_end)):
-            ref_seq = build_ref_seq(
-                self.tokenizer, logits[id, (start - 1):(end - 2), :])
-            orig_seq = self.tokenizer.decode(
-                input_ids[id, start:(end - 1)].detach().cpu())
-            self.wer_hist.append(calculate_wer([ref_seq], [orig_seq]))
-        if compute_result:
-            wer_ = np.mean(self.wer_hist)
-            self.wer_hist = []
-            return {'WER*': wer_}
-
-
-class WandbProgressCallback(WandbCallback):
-    def __init__(self,
-                 trainer: Trainer,
-                 tokenizer,
-                 wav_tokenizer: CustomWavTokenizer,
-                 train_dataset,
-                 val_dataset,
-                 num_samples=10):
-        super().__init__()
-        self.trainer = trainer
-        self.tokenizer = tokenizer
-        self.wav_tokenizer = wav_tokenizer
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.num_samples = num_samples
-
-    def _build_sample_table(self, sample_dataset):
-        output = self.trainer.predict(sample_dataset)
-        target = []
-        predicted = []
-        audio = []
-
-        for i, sample in enumerate(sample_dataset):
-            text_start, text_end = sample['label_text_start'], sample['label_text_end']
-            audio_start, audio_end = 1, text_start - 1
-            predicted_ = self.tokenizer.decode(
-                np.argmax(output.predictions[i, (text_start - 1):(text_end - 1), :], axis=-1))
-            audio_ = self.wav_tokenizer.decode(
-                sample['input_ids'][audio_start:audio_end] - WavQwenModel.AUDIO_TOKEN_THRESHOLD).numpy()
-            target_ = self.tokenizer.decode(
-                sample['input_ids'][text_start:text_end])
-            predicted.append(predicted_)
-            audio.append(audio_)
-            target.append(target_)
-
-        columns = ['Audio', 'Target', 'Predicted*']
-        data = [[wandb.Audio(audio, self.wav_tokenizer.WAV_SR), target, predicted]
-                for audio, target, predicted in zip(audio, target, predicted)]
-        sample_table = self._wandb.Table(data=data, columns=columns)
-        return sample_table
-
-    def _get_sample_ds(self, ds):
-        sample_ind = torch.randint(len(ds), size=(self.num_samples,))
-        sample_ds = [ds[ind] for ind in sample_ind]
-        return sample_ds
-
-    def log_val_table(self):
-        sample_ds = self._get_sample_ds(self.val_dataset)
-        sample_table = self._build_sample_table(sample_ds)
-        self._wandb.log({'Validation samples': sample_table})
-
-    def log_train_table(self):
-        sample_ds = self._get_sample_ds(self.train_dataset)
-        sample_table = self._build_sample_table(sample_ds)
-        self._wandb.log({'Train samples': sample_table})
-
-    def on_evaluate(self, args, state, control, **kwargs):
-        super().on_evaluate(args, state, control, **kwargs)
-        self.log_val_table()
-        self.log_train_table()
 
 
 def main():
@@ -341,7 +216,7 @@ def main():
         # gradient_checkpointing=True
     )
 
-    trainer = CustomTrainer(
+    trainer = WavAudioTrainer(
         model=qwen_model.model,
         args=train_args,
         data_collator=DataCollatorWithPadding(tokenizer),
@@ -349,7 +224,7 @@ def main():
         compute_metrics=wer_metric,
     )
 
-    wandb_callback = WandbProgressCallback(
+    wandb_callback = WandbAudioSampleCallback(
         trainer,
         tokenizer,
         wav_tokenizer,
