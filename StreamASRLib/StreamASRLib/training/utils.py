2c1abfc07f805aa9b0c10c2732accdf52b96fb1d
netkex <dp.3481088@gmail.com>
2025-03-18 03:09:59 +0300

    [WIP] Add reading config fun to utils

--- StreamASRLib/StreamASRLib/training/utils.py
+++ StreamASRLib/StreamASRLib/training/utils.py
@@ -1,6 +1,13 @@
 import torch
+import json
+from munch import Munch
 
 
 def build_ref_seq(tokenizer, output: torch.Tensor) -> str:
     tokens = torch.argmax(output, dim=-1)
     return tokenizer.decode(tokens.cpu())
+
+
+def read_config(path: str):
+    with open(path, "r") as config_file:
+        return Munch.fromDict(json.load(config_file))
